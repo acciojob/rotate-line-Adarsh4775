07f1ce589b0e54e66aa83089cef220ba4be101c1
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-25 12:32:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+function rotateLine(){
+    let unDiv = document.createElement("div");
+    unDiv.id = "line";
+	 unDiv.style.backgroundColor = "black";
+	 unDiv.style.width = "200px";
+     unDiv.style.height = "2px";
+     unDiv.style.position = "absolute";
+	 unDiv.style.top = "50%";
+	 unDiv.style.left = "50%";
+	unDiv.style.transformOrigin = "0% 50%";
+	unDiv.style.animation = "rotateLine 2s linear infinite";
+	document.body.appendChild(unDiv);
+
+	let styleSheet = document.styleSheets[0];
+	styleSheet.insertRule(  '@keyframes rotateLine {\
+						  0%{\
+		                       transform : rotate(0deg);\
+						  }\
+                          100%{\
+                               transform : rotate(360deg);\
+						  }\
+						  }', styleSheet.cssRules.length);
+}
+rotateLine();
